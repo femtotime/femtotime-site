bc8e5dbf773cdd219f747e3ec674fc61fff4c01a
femtotime <femtotimeltd@gmail.com>
2025-05-05 15:03:01 +0100

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-import tailwindcss from '@tailwindcss/postcss';
-
 export default {
-  plugins: [
-    tailwindcss(),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
